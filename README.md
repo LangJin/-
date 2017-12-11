f01945eb43d5d5d6cc8c1d635d56b831f7b3b4e1
langjin <fenyukuang@gmail.com>
2017-12-12 02:35:33 +0800

    flash

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # FlaskWebApp
 写一个个人博客
 ## 导入的包
-flask
-flask-bootstrap
-flask-moment
+flask  
+flask-bootstrap  
+flask-moment  
+flask-wtf  
+flask-script  
--- demo.py
+++ demo.py
@@ -4,18 +4,31 @@
 作者：浪晋
 说明：flask的第一个文件
 '''
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import Form
+from flask_script import Manager
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 from datetime import datetime
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('/client/index.html', current_time=datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+            session['name'] = form.name.data
+            return redirect(url_for('index'))
+    return render_template('/client/index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
@@ -33,5 +46,11 @@ def internal_server_error(e):
     return render_template('/errors/500.html'), 500
 
 
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
+    # app.run(debug=True)
