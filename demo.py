deb801edeea1c5aecb8690b154b18735828769f1
Bai <fenyukuang@gmail.com>
2017-12-12 17:20:48 +0800

    create db table

--- demo.py
+++ demo.py
@@ -93,6 +93,25 @@ def make_shell_context():
 manager.add_command("shell", Shell(make_context=make_shell_context))
 
 
+@manager.command
+def init_datas():
+    admin_role = Role(name='Admin')
+    mod_role = Role(name='Moderator')
+    user_role = Role(name='User')
+    user_john = User(username='john', role=admin_role)
+    user_susan = User(username='susan', role=user_role)
+    user_david = User(username='david', role=user_role)
+    db.session.add_all(
+        [admin_role, mod_role, user_role, user_john, user_susan, user_david])
+    db.session.commit()
+
+
+@manager.command
+def init_db():
+    db.drop_all()
+    db.create_all()
+
+
 if __name__ == '__main__':
     manager.run()
     # app.run(debug=True)
--- oldapp/app/__init__.py
+++ oldapp/app/__init__.py
@@ -5,6 +5,7 @@ from config import config
 
 db = SQLAlchemy()
 
+
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
@@ -12,5 +13,4 @@ def create_app(config_name):
     # 蓝图
     from .views import hviews
     app.register_blueprint(hviews)
-
     return app
