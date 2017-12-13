6af71890f65c6f144ec975391349b86a9473872e
Bai <fenyukuang@gmail.com>
2017-12-13 14:08:49 +0800

    改写OK

--- Albatron/application/main/views.py
+++ Albatron/application/main/views.py
@@ -25,7 +25,7 @@ def index():
             session['known'] = True
         session['name'] = form.name.data
         form.name.data = ''
-        return redirect(url_for('index'))
+        return redirect(url_for('.index'))
     return render_template(
         '/client/index.html',
         form=form,
