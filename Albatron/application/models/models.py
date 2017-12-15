5d42364f235e2d69de1b5dc7b795bda9631dfea0
Bai <fenyukuang@gmail.com>
2017-12-15 20:14:20 +0800

    初始化数据库

--- Albatron/application/models/models.py
+++ Albatron/application/models/models.py
@@ -13,6 +13,15 @@ class Role(db.Model):
     name = db.Column(db.String(64), unique=True)
     users = db.relationship('User', backref='role', lazy='dynamic')
 
+    @staticmethod
+    def initrole():
+        admin_role = Role(name='Admin')
+        mod_role = Role(name='Moderator')
+        user_role = Role(name='User')
+        db.session.add_all(
+            [admin_role, mod_role, user_role])
+        db.session.commit()
+
     def __repr__(self):
         return '<Role %r>' % self.name
 
@@ -22,6 +31,15 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    @staticmethod
+    def inituser():
+        user_john = User(username='john', role_id=1)
+        user_susan = User(username='susan', role_id=1)
+        user_david = User(username='david', role_id=3)
+        db.session.add_all(
+            [user_john, user_susan, user_david])
+        db.session.commit()
 
     def __repr__(self):
         return '<User %r>' % self.username
